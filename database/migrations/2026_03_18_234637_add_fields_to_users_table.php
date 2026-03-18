6cec79c32fe71eddf4e148a6b9f82112581756a1
Bebík <133703817+Bebicek@users.noreply.github.com>
2026-03-19 00:47:26 +0100

    Create 2026_03_18_234637_add_fields_to_users_table.php

---
+++ database/migrations/2026_03_18_234637_add_fields_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('bank_number')->nullable()->after('password');
+            $table->string('bank_code')->nullable()->after('bank_number');
+            $table->boolean('enabled')->default(true)->after('bank_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['bank_number', 'bank_code', 'enabled']);
+        });
+    }
+};
