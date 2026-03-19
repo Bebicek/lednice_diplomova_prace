6e471121ca62ba926541477572bbc6401aa77c79
Bebík <133703817+Bebicek@users.noreply.github.com>
2026-03-19 01:28:17 +0100

    added Order.php model

---
+++ app/Models/Order.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_amount',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'total_amount' => 'integer',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function debt(): HasOne
+    {
+        return $this->hasOne(Debt::class);
+    }
+
+    public function getTotalInCzkAttribute(): float
+    {
+        return $this->total_amount / 100;
+    }
+}
