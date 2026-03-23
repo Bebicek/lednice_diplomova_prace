fab2c9cc95b655f00c9116be11287402d490b07c
Bebík <133703817+Bebicek@users.noreply.github.com>
2026-03-23 23:00:20 +0100

    fix table dropdown not closing

--- resources/views/components/common/table-dropdown.blade.php
+++ resources/views/components/common/table-dropdown.blade.php
@@ -30,8 +30,8 @@
     </div>
 
     <div class="z-50 fixed" x-ref="content">
-        <div x-show="isOpen" x-cloak class="p-2 bg-white border border-gray-200 rounded-2xl shadow-lg dark:border-gray-800 dark:bg-gray-dark w-max">
-            <div class="space-y-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+        <div x-show="isOpen" x-cloak class="p-2 bg-white border border-gray-200 rounded-2xl shadow-lg dark:border-gray-800 dark:bg-gray-dark min-w-32 w-max">
+            <div @click="isOpen = false" class="space-y-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                 {{ $content }}
             </div>
         </div>
