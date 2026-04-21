1efd4f37b11850ff19c5ac601316b2a56d7a8707
Bebík <133703817+Bebicek@users.noreply.github.com>
2026-04-21 04:38:23 +0200

    auto-create storage dirs in entrypoint

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -9,4 +9,10 @@ if [ -d /var/www/html-image/public ]; then
     echo "[entrypoint] Done."
 fi
 
+mkdir -p /var/www/html/storage/framework/views \
+         /var/www/html/storage/framework/cache \
+         /var/www/html/storage/framework/sessions \
+         /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
 exec "$@"
