96fb730a9de9f7a9f7c83a2f6e48c8163757873d
Bebík <133703817+Bebicek@users.noreply.github.com>
2026-04-24 07:00:09 +0200

    update cart-counter and debt-counter to add there static number

--- resources/views/livewire/debt-counter.blade.php
+++ resources/views/livewire/debt-counter.blade.php
@@ -1,7 +1,3 @@
-<span>
-    @if($count > 0)
-        <span class="inline-flex items-center justify-center w-5 h-5 rounded-full bg-red-500 text-white text-xs font-bold">
-            {{ $count }}
-        </span>
-    @endif
+<span class="inline-flex items-center justify-center w-5 h-5 rounded-full text-xs font-bold {{ $count > 0 ? 'bg-red-500 text-white' : 'text-gray-400 dark:text-gray-500' }}">
+    {{ $count }}
 </span>
